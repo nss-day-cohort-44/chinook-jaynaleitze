4ab0564ae26a5e8ecd7b83ea7b100c2743ced68e
Jayna Leitze <jayna.leitze@gmail.com>
2021-02-25 13:54:20 -0600

    country list

---
+++ brazil_customers_invoices.sql
@@ -0,0 +1,10 @@
+Select 
+    c.FirstName,
+    c.LastName,
+    i.InvoiceId,
+    i.InvoiceDate,
+    i.BillingCountry
+    FROM Customer c
+    JOIN Invoice i
+    ON c.CustomerId = i.CustomerId
+    WHERE c.Country = 'Brazil';
---
+++ unique_invoice_countries.sql
@@ -0,0 +1,2 @@
+Select DISTINCT BillingCountry
+From Invoice 
