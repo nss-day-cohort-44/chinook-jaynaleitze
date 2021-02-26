323c209984536f989198fb0cbd93a4d9c8e15cc3
Jayna Leitze <jayna.leitze@gmail.com>
2021-02-26 09:19:09 -0600

    total_invoices_year

--- total_invoices_{year}.sql
+++ total_invoices_{year}.sql
@@ -1,7 +1,12 @@
-Select YEAR(i.InvoiceDate),
-        Sum(i.InvoiceId)    
+Select i.InvoiceDate, Count(i.InvoiceId)
 From Invoice i
-WHERE YEAR = 2009
+WHERE i.InvoiceDate Like "2009%" or i.InvoiceDate Like "2011%"
+GROUP BY i.InvoiceDate like "2009%"
 
-
+SELECT
+    Count(i.InvoiceId) NumberOfInvoices,
+    STRFTIME('%Y', i.InvoiceDate) InvoiceYear
+FROM Invoice i
+WHERE i.InvoiceDate Like "2009%" or i.InvoiceDate Like "2011%"
+GROUP BY InvoiceYear
 
